e91643ee9815bc607132b36079b704d4bce8f1e8
s-wucar <s-wucar@53S50030665.it.bsd405.org>
2019-11-15 22:48:34 -0800

    Checkpoint 2

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -9,55 +9,67 @@ import java.util.*;
 public class FracCalc {
 
 	public static void main(String[] args) {
-		// TODO: Read the input from the user and call produceAnswer with an equation
+
 		Scanner userInput = new Scanner(System.in);
+		String input = "";
 
 		System.out.println("Hi! This is FracCalc! Type \"quit\" to quit.");
-		System.out.print("Enter an equation: ");
 
-		String input = userInput.nextLine();
-		
-		System.out.println(produceAnswer(input));
+		while (!("quit   ".equals(input))) {
+			System.out.print("Enter an equation: ");
+			input = userInput.nextLine();
+			input = input + "   ";
+			System.out.println(produceAnswer(input));
+
+		}
 
 	}
 
 	public static String produceAnswer(String input) { // returns the answer
-		
-		String firstNumStr=firstNum(input);		
-		String firstWhole=fracSplitWhole(firstNumStr);
-		String firstNumerator=fracSplitNumerator(firstNumStr);
-		String firstDenominator=fracSplitDenominator(firstNumStr);
-		
-		String secondNumStr=secondNum(input);		
-		String secondWhole=fracSplitWhole(secondNumStr);
-		String secondNumerator=fracSplitNumerator(secondNumStr);
-		String secondDenominator=fracSplitDenominator(secondNumStr);
-		
-		String temporaryAnswer="whole:"+secondWhole+" numerator:"+secondNumerator+" denominator:"+secondDenominator;
-		
+
+		String firstNumStr = firstNum(input);
+		String firstWhole = fracSplitWhole(firstNumStr);
+		String firstNumerator = fracSplitNumerator(firstNumStr);
+		String firstDenominator = fracSplitDenominator(firstNumStr);
+
+		String secondNumStr = secondNum(input);
+		String secondWhole = fracSplitWhole(secondNumStr);
+		String secondNumerator = fracSplitNumerator(secondNumStr);
+		String secondDenominator = fracSplitDenominator(secondNumStr);
+
+		String temporaryAnswer;
+
+		if ("quit   ".equals(input)) {
+			temporaryAnswer = "You've quit >:(";
+		} else {
+			temporaryAnswer = "whole:" + secondWhole + " numerator:" + secondNumerator + " denominator:"
+					+ secondDenominator;
+		}
+
 		return temporaryAnswer;
 	}
-	
-	public static String firstNum(String input) { //returns the entire first number
+
+	public static String firstNum(String input) { // returns the entire first number
+
 		int spaceIndex = input.indexOf(" ");
 		String firstNumStr = input.substring(0, spaceIndex);
-		
+
 		return firstNumStr;
 	}
-	
-	public static String secondNum(String input) { //returns the entire second number
+
+	public static String secondNum(String input) { // returns the entire second number
 
 		int spaceIndex = input.indexOf(" ");
 		String secondNumStr = input.substring(spaceIndex + 3);
-		
+
 		return secondNumStr;
 	}
-	
-	public static String operand(String input) { //returns the operand
+
+	public static String operand(String input) { // returns the operand
 
 		int spaceIndex = input.indexOf(" ");
 		String operand = input.substring(spaceIndex + 1, spaceIndex + 2);
-		
+
 		return operand;
 	}
 
@@ -66,16 +78,15 @@ public class FracCalc {
 		int fracIndex = firstNumStr.indexOf("_");
 		String whole = "";
 
-		if (fracIndex != -1) { //for x_y/z
+		if (fracIndex != -1) { // for x_y/z
 			whole = firstNumStr.substring(0, fracIndex);
 		}
 
-		else { //for y/z and x
-			int slashIndex=firstNumStr.indexOf("/");
-			if (slashIndex!=-1) { //for y/z, returns whole=0
-				whole="0";
-			}
-			else { //for x, returns whole=x
+		else { // for y/z and x
+			int slashIndex = firstNumStr.indexOf("/");
+			if (slashIndex != -1) { // for y/z, returns whole=0
+				whole = "0";
+			} else { // for x, returns whole=x
 				whole = firstNumStr;
 			}
 		}
@@ -84,7 +95,8 @@ public class FracCalc {
 
 	}
 
-	public static String fracSplitNumerator(String firstNumStr) { // if there is a fraction component, returns the numerator
+	public static String fracSplitNumerator(String firstNumStr) { // if there is a fraction component, returns the
+																	// numerator
 
 		int fracIndex = firstNumStr.indexOf("_");
 		String numerator = "";
@@ -108,29 +120,53 @@ public class FracCalc {
 
 	}
 
-	public static String fracSplitDenominator(String firstNumStr) { //if there is a fraction component, returns the denominator
-    	
-    	int fracIndex = firstNumStr.indexOf("_");
-    	String denominator="";
-    	
-    	if (fracIndex!=-1) { //for x_y/z, returns z
-    		String frac=firstNumStr.substring(fracIndex+1);
-    		int slashIndex=frac.indexOf("/");
-    		denominator=frac.substring(slashIndex+1);
-    	}
-    	
-    	else { //either y/z or x
-    		int slashIndex=firstNumStr.indexOf("/");
-    		if (slashIndex!=-1) { //for y/z, returns z
-    			denominator=firstNumStr.substring(slashIndex+1);
-    		}
-    		else { //for x, returns denominator=1
-    			denominator="1";
-    		}
-    	
-    	}
-    	return denominator;
+	public static String fracSplitDenominator(String firstNumStr) { // if there is a fraction component, returns the
+																	// denominator
+
+		int fracIndex = firstNumStr.indexOf("_");
+		String denominator = "";
+
+		if (fracIndex != -1) { // for x_y/z, returns z
+			String frac = firstNumStr.substring(fracIndex + 1);
+			int slashIndex = frac.indexOf("/");
+			denominator = frac.substring(slashIndex + 1);
+		}
+
+		else { // either y/z or x
+			int slashIndex = firstNumStr.indexOf("/");
+			if (slashIndex != -1) { // for y/z, returns z
+				denominator = firstNumStr.substring(slashIndex + 1);
+			} else { // for x, returns denominator=1
+				denominator = "1";
+			}
+
+		}
+		return denominator;
 	}
-     
-    
+
+	public static String newNumerator(String whole, String numerator, String denominator) { // whole*denom+num
+		int wholeInt = Integer.parseInt(whole);
+		int numeratorInt = Integer.parseInt(numerator);
+		int denominatorInt = Integer.parseInt(denominator);
+
+		int newNumeratorInt = wholeInt * denominatorInt + numeratorInt;
+
+		String newNumerator = "" + newNumeratorInt;
+
+		return newNumerator;
+	}
+
+	public static String multiplication(String firstNumStr, String secondNumStr) {
+		String firstNumStr = firstNum(input);
+		String firstWhole = fracSplitWhole(firstNumStr);
+		String firstNumerator = fracSplitNumerator(firstNumStr);
+		String firstDenominator = fracSplitDenominator(firstNumStr);
+
+		String secondNumStr = secondNum(input);
+		String secondWhole = fracSplitWhole(secondNumStr);
+		String secondNumerator = fracSplitNumerator(secondNumStr);
+		String secondDenominator = fracSplitDenominator(secondNumStr);
+	}
+
+
 }
