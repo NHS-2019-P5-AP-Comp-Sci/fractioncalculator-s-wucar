2e1c94bcd8613d123e69a7e95a0510ac41c1da37
s-wucar <s-wucar@53S50030665.it.bsd405.org>
2019-11-15 18:46:01 -0800

    Checkpoint 1

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -4,27 +4,57 @@
 
 package fracCalc;
 
+import java.util.*;
+
 public class FracCalc {
 
     public static void main(String[] args)
     {
         // TODO: Read the input from the user and call produceAnswer with an equation
-
+    	Scanner userInput = new Scanner(System.in);
+    	
+    	System.out.println("Hi! This is FracCalc! Type \"quit\" to quit.");
+    	System.out.print("Enter an equation: ");
+    	
+		String input = userInput.nextLine();
+		
+		System.out.println(produceAnswer(input));
+    	   	
     }
 
-    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-    // This function takes a String 'input' and produces the result
-    //
-    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-    //      e.g. input ==> "1/2 + 3/4"
-    //
-    // The function should return the result of the fraction after it has been calculated
-    //      e.g. return ==> "1_1/4"
+    
+    
     public static String produceAnswer(String input)
     {
-        // TODO: Implement this function to produce the solution to the input
+    	int spaceIndex=input.indexOf(" ");
+    	String firstNumStr=input.substring(0,spaceIndex);
+    	String secondNumStr=input.substring(spaceIndex+3);
+    	String operand=input.substring(spaceIndex+1,spaceIndex+2);
+    	
+    	//int i = Integer.parseInt(myString);
+    	//int firstNum=Integer.parseInt(firstNumStr);
+    	//int secondNum=Integer.parseInt(secondNumStr);
+    	
+    	//String str1 = Integer.toString(a);
+    	
+    	   	
+		/*int fracIndex = input.indexOf("_");
 
-        return "";
+		if (fracIndex != -1) {
+
+			String wholeNumString = input.substring(0, fracIndex - 1);
+			String frac = input.substring(fracIndex);
+			int slashIndex = frac.indexOf("/");
+			String numString = frac.substring(0, slashIndex - 1);
+			String denomString = frac.substring(slashIndex);
+
+			int wholeNum = Integer.parseInt(wholeNumString);
+			int num = Integer.parseInt(numString);
+			int denom = Integer.parseInt(denomString);   
+		}*/
+    	
+    	
+        return secondNumStr;
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
