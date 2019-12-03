dd787efd63606738ab428ec4a195f5d3ca5d530e
s-wucar <s-wucar@53S50030665.it.bsd405.org>
2019-12-03 01:13:31 -0800

    Checkpoint 3

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -27,7 +27,7 @@ public class FracCalc {
 
 	public static String produceAnswer(String input) { // returns the answer
 
-		String firstNumStr = firstNum(input);
+		/*String firstNumStr = firstNum(input);
 		String firstWhole = fracSplitWhole(firstNumStr);
 		String firstNumerator = fracSplitNumerator(firstNumStr);
 		String firstDenominator = fracSplitDenominator(firstNumStr);
@@ -35,18 +35,31 @@ public class FracCalc {
 		String secondNumStr = secondNum(input);
 		String secondWhole = fracSplitWhole(secondNumStr);
 		String secondNumerator = fracSplitNumerator(secondNumStr);
-		String secondDenominator = fracSplitDenominator(secondNumStr);
-
-		String temporaryAnswer;
-
-		if ("quit   ".equals(input)) {
-			temporaryAnswer = "You've quit >:(";
-		} else {
-			temporaryAnswer = "whole:" + secondWhole + " numerator:" + secondNumerator + " denominator:"
-					+ secondDenominator;
+		String secondDenominator = fracSplitDenominator(secondNumStr);*/
+		
+		String firstNumStr = firstNum(input);
+		String secondNumStr = secondNum(input);
+		String something ="";
+		
+		String operand=operand(input);
+		//if ("quit   ".equals(input))
+		if("*".equals(operand)) {
+			something=multiplication(input, firstNumStr, secondNumStr);
 		}
-
-		return temporaryAnswer;
+		if("/".equals(operand)) {
+			something=division(input, firstNumStr, secondNumStr);
+		}
+		if("+".equals(operand)) {
+			something=addition(input, firstNumStr, secondNumStr);
+		}
+		if("-".equals(operand)) {
+			something=subtraction(input, firstNumStr, secondNumStr);
+		}
+		if ("quit   ".equals(input)) {
+			something = "You've quit >:(";
+		}
+		
+		return something;
 	}
 
 	public static String firstNum(String input) { // returns the entire first number
@@ -57,6 +70,7 @@ public class FracCalc {
 		return firstNumStr;
 	}
 
+	
 	public static String secondNum(String input) { // returns the entire second number
 
 		int spaceIndex = input.indexOf(" ");
@@ -65,6 +79,7 @@ public class FracCalc {
 		return secondNumStr;
 	}
 
+	
 	public static String operand(String input) { // returns the operand
 
 		int spaceIndex = input.indexOf(" ");
@@ -73,6 +88,7 @@ public class FracCalc {
 		return operand;
 	}
 
+	
 	public static String fracSplitWhole(String firstNumStr) { // if there is a whole number component, returns it
 
 		int fracIndex = firstNumStr.indexOf("_");
@@ -95,6 +111,7 @@ public class FracCalc {
 
 	}
 
+	
 	public static String fracSplitNumerator(String firstNumStr) { // if there is a fraction component, returns the
 																	// numerator
 
@@ -120,6 +137,7 @@ public class FracCalc {
 
 	}
 
+	
 	public static String fracSplitDenominator(String firstNumStr) { // if there is a fraction component, returns the
 																	// denominator
 
@@ -144,7 +162,12 @@ public class FracCalc {
 		return denominator;
 	}
 
-	public static String newNumerator(String whole, String numerator, String denominator) { // whole*denom+num
+	
+	public static String newNumerator(String whole, String numerator, String denominator) { // whole*denom+num FIX
+		
+		//String firstNumStr=firstNum(input);
+		//String whole=fracSplitWhole(firstNumStr);
+		
 		int wholeInt = Integer.parseInt(whole);
 		int numeratorInt = Integer.parseInt(numerator);
 		int denominatorInt = Integer.parseInt(denominator);
@@ -156,16 +179,138 @@ public class FracCalc {
 		return newNumerator;
 	}
 
-	public static String multiplication(String firstNumStr, String secondNumStr) {
-		String firstNumStr = firstNum(input);
+	
+	public static String multiplication(String input, String firstNumStr, String secondNumStr) { //returns the mixed, unsimplified product
+		
 		String firstWhole = fracSplitWhole(firstNumStr);
 		String firstNumerator = fracSplitNumerator(firstNumStr);
-		String firstDenominator = fracSplitDenominator(firstNumStr);
+		String firstDenominator = fracSplitDenominator(firstNumStr); //
+		String firstNewNumerator=newNumerator(firstWhole, firstNumerator, firstDenominator); //
 
-		String secondNumStr = secondNum(input);
 		String secondWhole = fracSplitWhole(secondNumStr);
 		String secondNumerator = fracSplitNumerator(secondNumStr);
-		String secondDenominator = fracSplitDenominator(secondNumStr);
+		String secondDenominator = fracSplitDenominator(secondNumStr); //
+		String secondNewNumerator=newNumerator(secondWhole, secondNumerator, secondDenominator); //
+		
+		//int wholeInt = Integer.parseInt(whole);
+		int firstNewNumeratorInt=Integer.parseInt(firstNewNumerator);
+		int firstDenominatorInt=Integer.parseInt(firstDenominator);
+		int secondNewNumeratorInt=Integer.parseInt(secondNewNumerator);
+		int secondDenominatorInt=Integer.parseInt(secondDenominator);
+		
+		int productNumerator=firstNewNumeratorInt*secondNewNumeratorInt;
+		int productDenominator=firstDenominatorInt*secondDenominatorInt;
+		
+		String mixedProduct=productNumerator+"/"+productDenominator;
+		
+		return mixedProduct;
+		
+	}
+
+	
+	public static String division(String input, String firstNumStr, String secondNumStr) { // returns the mixed, unsimplified quotient
+		String firstWhole = fracSplitWhole(firstNumStr);
+		String firstNumerator = fracSplitNumerator(firstNumStr);
+		String firstDenominator = fracSplitDenominator(firstNumStr); //
+		String firstNewNumerator=newNumerator(firstWhole, firstNumerator, firstDenominator); //
+
+		String secondWhole = fracSplitWhole(secondNumStr);
+		String secondNumerator = fracSplitNumerator(secondNumStr);
+		String secondDenominator = fracSplitDenominator(secondNumStr); //
+		String secondNewNumerator=newNumerator(secondWhole, secondNumerator, secondDenominator); //
+		
+		//int wholeInt = Integer.parseInt(whole);
+		int firstNewNumeratorInt=Integer.parseInt(firstNewNumerator);
+		int firstDenominatorInt=Integer.parseInt(firstDenominator);
+		int secondNewNumeratorInt=Integer.parseInt(secondNewNumerator);
+		int secondDenominatorInt=Integer.parseInt(secondDenominator);
+		
+		int quotientNumerator=firstNewNumeratorInt*secondDenominatorInt;
+		int quotientDenominator=firstDenominatorInt*secondNewNumeratorInt;
+		
+		String mixedQuotient=quotientNumerator+"/"+quotientDenominator;
+		
+		return mixedQuotient;
+	}
+
+	
+	public static String addition(String input, String firstNumStr, String secondNumStr) { //returns the mixed, unsimplified sum
+		String firstWhole = fracSplitWhole(firstNumStr);
+		String firstNumerator = fracSplitNumerator(firstNumStr);
+		String firstDenominator = fracSplitDenominator(firstNumStr); //
+		String firstNewNumerator=newNumerator(firstWhole, firstNumerator, firstDenominator); //
+
+		String secondWhole = fracSplitWhole(secondNumStr);
+		String secondNumerator = fracSplitNumerator(secondNumStr);
+		String secondDenominator = fracSplitDenominator(secondNumStr); //
+		String secondNewNumerator=newNumerator(secondWhole, secondNumerator, secondDenominator); //
+		
+		//int wholeInt = Integer.parseInt(whole);
+		int firstNewNumeratorInt=Integer.parseInt(firstNewNumerator);
+		int firstDenominatorInt=Integer.parseInt(firstDenominator); //lcm
+		int secondNewNumeratorInt=Integer.parseInt(secondNewNumerator);
+		int secondDenominatorInt=Integer.parseInt(secondDenominator); //lcm
+		
+		int gcf=gcf(firstDenominatorInt, secondDenominatorInt);
+		int commonDenominator=firstDenominatorInt*secondDenominatorInt/gcf;
+		
+		int firstNewNumeratorIntCommon=firstNewNumeratorInt*(commonDenominator/firstDenominatorInt);
+		int secondNewNumeratorIntCommon=secondNewNumeratorInt*(commonDenominator/secondDenominatorInt);
+		
+		String mixedSum=(firstNewNumeratorIntCommon+secondNewNumeratorIntCommon)+"/"+commonDenominator;
+		
+		return mixedSum;
+	}
+
+	
+	public static String subtraction(String input, String firstNumStr, String secondNumStr) { //returns the mixed, unsimplified difference
+		String firstWhole = fracSplitWhole(firstNumStr);
+		String firstNumerator = fracSplitNumerator(firstNumStr);
+		String firstDenominator = fracSplitDenominator(firstNumStr); //
+		String firstNewNumerator=newNumerator(firstWhole, firstNumerator, firstDenominator); //
+
+		String secondWhole = fracSplitWhole(secondNumStr);
+		String secondNumerator = fracSplitNumerator(secondNumStr);
+		String secondDenominator = fracSplitDenominator(secondNumStr); //
+		String secondNewNumerator=newNumerator(secondWhole, secondNumerator, secondDenominator); //
+		
+		//int wholeInt = Integer.parseInt(whole);
+		int firstNewNumeratorInt=Integer.parseInt(firstNewNumerator);
+		int firstDenominatorInt=Integer.parseInt(firstDenominator); //lcm
+		int secondNewNumeratorInt=Integer.parseInt(secondNewNumerator);
+		int secondDenominatorInt=Integer.parseInt(secondDenominator); //lcm
+		
+		int gcf=gcf(firstDenominatorInt, secondDenominatorInt);
+		int commonDenominator=firstDenominatorInt*secondDenominatorInt/gcf;
+		
+		int firstNewNumeratorIntCommon=firstNewNumeratorInt*(commonDenominator/firstDenominatorInt);
+		int secondNewNumeratorIntCommon=secondNewNumeratorInt*(commonDenominator/secondDenominatorInt);
+		
+		String mixedDifference=(firstNewNumeratorIntCommon-secondNewNumeratorIntCommon)+"/"+commonDenominator;
+		
+		return mixedDifference;
+	}
+	
+	
+	public static int gcf(int a, int b) {
+		/*public static int GCF(int a, int b) {
+    		if (b == 0) {
+      			return a;
+		 	} else {
+      		return (GCF(b, a % b));
+    	 	}
+  		}*/
+		
+		if (a==1 || b==1) {
+			if (a!=1) {
+				return a;
+			} else {
+				return b;
+			}
+		} else {
+			return gcf(b, a%b);
+		}
+		
 	}
 
 
